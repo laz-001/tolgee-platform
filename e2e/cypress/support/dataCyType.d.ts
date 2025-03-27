95894417819355ce730d9d6b829b8d7c08ad1415
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2025-03-27 16:17:23 +0100

    feat: implemented glossary create form

--- e2e/cypress/support/dataCyType.d.ts
+++ e2e/cypress/support/dataCyType.d.ts
@@ -102,6 +102,7 @@ declare namespace DataCy {
         "api-key-list-item-regenerate-button" |
         "api-keys-create-edit-dialog" |
         "api-keys-project-select-item" |
+        "assigned-projects-select" |
         "assignee-search-select-popover" |
         "assignee-select" |
         "auto-avatar-img" |
@@ -248,6 +249,7 @@ declare namespace DataCy {
         "global-plus-button" |
         "global-search-field" |
         "global-user-menu-button" |
+        "glossary-base-language-select" |
         "import-conflict-resolution-dialog" |
         "import-conflicts-not-resolved-dialog" |
         "import-conflicts-not-resolved-dialog-cancel-button" |
