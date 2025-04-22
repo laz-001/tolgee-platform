08c9e89fa255af2dffa646328b367406937ac4ae
Štěpán Granát <granat.stepan@gmail.com>
2025-04-22 14:10:51 +0200

    chore: ktlint

--- ee/backend/app/src/main/kotlin/io/tolgee/ee/data/AiPlaygroundResultRequest.kt
+++ ee/backend/app/src/main/kotlin/io/tolgee/ee/data/AiPlaygroundResultRequest.kt
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotEmpty
 data class AiPlaygroundResultRequest(
   @field:NotEmpty
   var keys: List<Long>? = null,
-
   @field:NotEmpty
   var languages: List<Long>? = null,
 )
